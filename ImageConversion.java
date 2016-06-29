33c8f7f1209705fd1edac3296a9534bf895d1252
DunnyDon <conordonohue@gmail.com>
2016-06-29 09:28:40 +0100

    BMP Conversion to jpg
    Run Script to convert BMP Images to JPEGs

--- ImageConversion.java
+++ ImageConversion.java
@@ -15,25 +15,30 @@ import javax.imageio.ImageIO;
 public class ImageConversion {
 
 	public static void main(String[] args) throws IOException {
+		//define your file location and your file destination
 		String path = "c:\\Users\\cdonohue\\Desktop\\Temp";
 		String destination = "c:\\Users\\cdonohue\\Documents\\IMGFolder\\";
+		//walk the path of where the files are located
 		Files.walk(Paths.get(path)).forEach(path_to_file -> {
 			if (Files.isRegularFile(path_to_file)) {
+				//check if files
 		        File inputFile = new File(path_to_file.toString());
 				int i = inputFile.getName().lastIndexOf('.');
 				if (i >= 0) {
 				    String ext = inputFile.getName().substring(i+1);
-				    System.out.println(ext);
+				    //System.out.println(ext);
+				    //debugging to check what kind of files they are
 				    if (Objects.equals(ext, "bmp")){
-				    	System.out.println("File is a "+ext);
-				    	System.out.println(path_to_file);
-				    	//Abpove this works and files being saved to the bmp folder are only bmps
-				    	//However Conversion to jpgs is failing miserably
-				    	File outputFile = new File(destination+inputFile.getName());
+				    	//System.out.println("File is a "+ext);
+				    	//System.out.println(path_to_file);
+				    	//^^Path to original file
+				    	//if file is jpg then create a new file with the same name but with jpg replacing bmp
+				    	File outputFile = new File((destination+inputFile.getName()).replace(".bmp",".jpg"));
 				        try (InputStream is = new FileInputStream(inputFile)) {
 							BufferedImage image = ImageIO.read(is);
 							try (OutputStream os = new FileOutputStream(outputFile)) {
-								ImageIO.write(image, "jpg", os);
+								ImageIO.write(image, "jpg", outputFile);
+								//write the image to that file
 							} catch (Exception exp) {
 								exp.printStackTrace();
 							}
